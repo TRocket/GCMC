0ab254d6693a96eb4651bf4546005cc8d39a356b
TRocket <TRocket@Itchypp.github>
2013-04-14 11:22:23 +0100

    works a bit more now...

--- src/server.cpp
+++ src/server.cpp
@@ -5,9 +5,75 @@
 
 using std::cout;
 using std::endl;
+using std::string;
 using boost::asio::ip::tcp;
+using namespace boost;
+
+char read_byte(tcp::socket &sock){
+	char data[1];
+	asio::read(sock, asio::buffer(data, 1));
+	return data[0];
+}
+
+short read_short(tcp::socket &sock){
+	unsigned char data[2];
+	asio::read(sock, asio::buffer(data, 2));
+	return (data[0]<<8) + data[1];
+}
+
+int read_int(tcp::socket &sock){
+	unsigned char data[4];
+	asio::read(sock, asio::buffer(data, 4));
+	return (data[0]<<24) + (data[1]<<16) + (data[2]<<8) + data[3];
+}
+
+string read_string(tcp::socket &sock){
+	short str_len = read_short(sock);
+	cout << "string length: " << str_len << "\n";
+	flush(cout);
+	char str[str_len];
+	int i;
+	for (i = 0; i < str_len; ++i) {
+					str[i] = read_short(sock);
+				}
+	return str;
+}
+
+void write_byte(tcp::socket &sock, char bytedata){
+	unsigned char data[1];
+	data[0] = bytedata;
+	asio::write(sock, asio::buffer(data, 1));
+}
+
+void write_short(tcp::socket &sock, short shortdata){
+	unsigned char data[2];
+	data[0] = shortdata;
+	data[1] = shortdata >> 8 ;
+	cout << "data: " << data << endl;
+	asio::write(sock, asio::buffer(data, sizeof(data)));
+}
+
+void write_int(tcp::socket &sock, int intdata){
+	unsigned char data[4];
+	data[0] = intdata;
+	data[1] = intdata >> 8;
+	data[2] = intdata >> 16;
+	data[3] = intdata >> 24;
+	asio::write(sock, asio::buffer(data));
+}
+
+void write_string(tcp::socket &sock, string str){
+	short str_len = str.length();
+	str_len = 4;
+	write_short(sock, str_len);
+	str[str_len];
+	for (int i = 0; i < str_len; ++i) {
+		write_byte(sock, str[i]);
+	}
+}
 
 int main(){
+	cout << sizeof(char) << endl;
 	cout << "gccraftbukkit\n";
 	flush(cout);
 	boost::asio::io_service io_service;
@@ -16,30 +82,46 @@ int main(){
 	      tcp::socket socket(io_service);
 	      acceptor.accept(socket);
 	      cout << "got connection\n";
-	      	flush(cout);
-	      boost::system::error_code ignored_error;
-	      boost::array<char, 128> buf;
+	      flush(cout);
 	      boost::system::error_code error;
 
-	      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-	      switch (buf[0]){
+	      //size_t len = socket.read_some(boost::asio::buffer(buf), error);
+	      short protocol_ver;
+	      short str_len;
+	      char packetID = read_byte(socket);
+	      std::string username_str;
+	      std::string serveraddr_str;
+	      int port;
+	      cout << "packet ID" << std::hex << packetID << endl;
+	      switch (packetID){
 	      case 0x02:
-	    	  short protocol_ver = buf[1];
+	    	  protocol_ver = read_byte(socket);
 	    	  cout << "protocol version: " << protocol_ver << endl;
-	    	  short str_len = buf[2]+buf[3];
-	    	  cout << "string length: " << str_len << endl;
-	    	  char username_str[str_len];
-	    	  int i;
-	    	  for (i = 0; i < str_len; ++i) {
-				username_str[i] = buf[(i*2)+4]+buf[(i*2)+5];
-			}
+	    	  username_str = read_string(socket);
 	    	  cout << username_str << endl;
+	    	  serveraddr_str = read_string(socket);
+	    	  cout << serveraddr_str << endl;
+	    	  port = read_int(socket);
+	    	  cout << port << endl;
+
+	    	  //write_byte(socket, 0xFD);
+	    	  write_string(socket, "GCMC");
+	    	  write_short(socket, 5);
+	    	  write_byte(socket, 0xA);write_byte(socket, 0xB);write_byte(socket, 0xC);write_byte(socket, 0xD);write_byte(socket, 0xE);
+	    	  write_short(socket, 4);
+	    	  write_byte(socket, 0xF);write_byte(socket, 0x11);write_byte(socket, 0xF);write_byte(socket, 0x11);
+
+	    	  break;
+	      default:
+	    	  cout << packetID;
+	    	  cout << " unknown packet id\n";
 
 	      }
-	      BOOST_FOREACH(char byte, buf){
-		      printf("%x ",byte);
-	      }
-	      cout<<"\n";
+
+	      //BOOST_FOREACH(char byte, buf){
+		  //    printf("%x ",byte);
+	      //}
+	      //cout<<"\n";
 
 	    }
 
